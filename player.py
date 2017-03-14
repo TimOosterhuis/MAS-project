db58caf0ebae4554f6863d79d7751844f41a5d90
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-03-14 12:18:02 +0100

    started with game setup/rules

--- player.py
+++ player.py
@@ -1,3 +1,7 @@
+import random
+from rules_config import playable
+
+
 
 class Player:
     def __init__(self, team, c_cards, o_cards, turn):
@@ -7,5 +11,6 @@ class Player:
         self.turn = turn
         # en dan self knowledge = get_knowledge_from(open_cards, closed_cards)
 
-    def play_card(self):
-        pass
+    def play_card(self, trick = None):
+        cards = playable(self.open_cards + self.closed_cards, self.team, trick)
+        return cards[random.randint(len(cards))]
--- rules_config.py
+++ rules_config.py
@@ -16,3 +16,25 @@ NUM_ROUNDS = len(SUITS) * len(NORMAL_POINTS) // NUM_PLAYERS
 
 CLOSED_CARDS = NUM_ROUNDS // 2
 OPEN_CARDS = NUM_ROUNDS - CLOSED_CARDS
+
+def playable(cards, team, trick = None):
+    if trick:
+        trump_cards = [card for card in cards if card[0] == trick.trump]
+        if trick.high_card[2] != trick.trump:
+            suit_cards = [card for card in cards if card[0] == trick.high_card[0]]
+            if suit_cards != []:
+                playable_cards = suit_cards
+            else:
+                if trick.winner.team != team and trump_cards != []:
+                    playable_cards = trump_cards
+                else:
+                    playable_cards = cards
+        else:
+            high_trump_cards = [card for card in trump_cards if card[2] > trick.high_card]
+            if high_trump_cards != []:
+                playable_cards = high_trump_cards
+            else:
+                playable_cards = cards
+    else:
+        playable_cards = cards
+    return playable_cards
