d7b60964f8082b143d19b93c29580a7678e03e87
Rogier de Weert <rogier92@gmail.com>
2017-03-14 15:50:14 +0100

    small changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# from rules_config import *
-from rules_config_big import *
+from rules_config import *
+#  from rules_config_big import *
 from player import Player
 from trick import Trick
 import random
@@ -17,11 +17,24 @@ def main():
     # print(cards)
     # initialize players
     assert NUM_PLAYERS == 4
-    player_1 = Player('1', [], [], 0, 'South')
-    player_2 = Player('2', [], [], 1, 'East')
-    player_3 = Player('1', [], [], 2, 'North')
-    player_4 = Player('2', [], [], 3, 'West')
-    players = [player_1, player_2, player_3, player_4]
+    player_1 = Player('1', 0, 'South')
+    player_2 = Player('2', 1, 'East')
+    player_3 = Player('1', 2, 'North')
+    player_4 = Player('2', 3, 'West')
+
+    #Randomize first starter
+    lst = [0,1,2,3]
+    random.shuffle(lst)
+    players = [None,None,None,None]
+    players[lst[0]] = player_1
+    players[lst[1]] = player_2
+    players[lst[2]] = player_3
+    players[lst[3]] = player_4
+
+    # Old, player is always first starter here
+    #players = [player_1, player_2, player_3, player_4]
+
+    common_knowledge = []
     idx = 0
     idx2 = CLOSED_CARDS
     #assert CLOSED_CARDS == OPEN_CARDS
@@ -31,22 +44,54 @@ def main():
         idx = idx2
         idx2 += OPEN_CARDS
         player.open_cards = cards[idx:idx2]
+        for open_card in player.open_cards:
+            common_knowledge.append((player.name, open_card, 0))
         idx = idx2
         idx2 += CLOSED_CARDS
-        print(str(player.name) + ' has ' + str(player.closed_cards) + ' and ' + str(player.open_cards))
-    #     print(player.open_cards)
-    # print(player_4.open_cards)
+        #print(str(player.name) + ' has ' + str(player.closed_cards) + ' and ' + str(player.open_cards))
+        print(str(player.name) + ' has ' + str(len(player.closed_cards)) + ' closed cards and ' + str(
+            len(player.open_cards)) + ' open cards')
+
+
+    for player in players:
+        player.knowledge.extend(common_knowledge)
+        for closed_card in player.closed_cards:
+            player.knowledge.append((player.name, closed_card, 0))
+        print player.name, "knows ",player.knowledge
+
+
 
     score = {'1' : 0, '2' : 0}
     for round in range(NUM_ROUNDS):
         print('\nnew round')
+
         # First player plays a card here
         trick = Trick(trump, players[0], players[0].play_card())
         print(str(players[0].name) + ' plays ' + str(trick.cards[-1]))
+        for player in players:
+            try:
+                player.knowledge.remove((players[0].name, trick.cards[-1], 0))
+            except ValueError:
+                pass
+            player.knowledge.append((players[0].name, trick.cards[-1],1))
+
+
         # Each following player picks card to play and plays
         for player in players[1:]:
             trick.add_card(player, player.play_card(trick))
             print(str(player.name) + ' plays ' + str(trick.cards[-1]))
+
+            print('\n--Here all Knowledge is updated--\n')
+            for pla in players:
+                try:
+                    pla.knowledge.remove((player.name, trick.cards[-1], 0))
+                except ValueError:
+                    pass
+                pla.knowledge.append((player.name, trick.cards[-1], 1))
+                #print pla.knowledge
+
+
+
         score[trick.winner.team] += trick.score
         print(str(trick.winner.name) + ' wins the trick with highest card ' + str(trick.high_card) + ', trick score ' + str(trick.score))
         players = players[trick.winner.turn:] + players[:trick.winner.turn]
--- player.py
+++ player.py
@@ -4,17 +4,20 @@ from rules_config import playable
 
 
 class Player:
-    def __init__(self, team, c_cards, o_cards, turn, name):
+    def __init__(self, team, turn, name):
         self.team = team
-        self.closed_cards = c_cards
-        self.open_cards = o_cards
+        self.closed_cards = []
+        self.open_cards = []
         self.turn = turn
         self.name = name
+        self.knowledge = []
         # en dan self knowledge = get_knowledge_from(open_cards, closed_cards)
 
     def play_card(self, trick = None):
         cards = playable(self.open_cards + self.closed_cards, self.team, trick)
         print('playable cards for ' + self.name + ' are ' + str(cards))
+
+        print("\n---HERE A PLAYER NEEDS TO DEVISE ITS BEST STRATEGY---\n")
 
         card = cards[random.randint(0, len(cards) - 1)]
         if card in self.open_cards:
