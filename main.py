87b0ac171ee267220845b650fafb2ff0967559a0
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-03-14 09:48:26 +0100

    started with game setup/rules

---
+++ main.py
@@ -0,0 +1,9 @@
+from rules_config import *
+import player
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ rules_config.py
@@ -0,0 +1,9 @@
+SUITS = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+NORMAL_POINTS = {'nine' : 0,
+                 'jack' : 2,
+                 'ten' : 10,
+                 'ace' : 11}
+TRUMP_POINTS = {'nine' : 14,
+                 'jack' : 20,
+                 'ten' : 10,
+                 'ace' : 11}
