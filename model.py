6bfd4b7c29dd28856b82383190513021c21c17b1
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-04-01 11:00:46 +0200

    relations between non true worlds and legend update

--- model.py
+++ model.py
@@ -35,10 +35,13 @@ REL_LINE_START = {
 
 
 def draw_model(screen, players, font, card, left, top, width, height):
-
+    legend = font.render("true world: ", 1, (0, 0, 0))
+    screen.blit(legend, (left + 50, top + 60))
+    legend_rect = pygame.Rect(left + 115, top + 65, 12, 12)
+    pygame.draw.rect(screen, (225, 225, 0), legend_rect, 0)
     for player in players:
         legend = font.render(player.name, 1, REL_COLOR_DICT[player.name])
-        screen.blit(legend, (left + 50, top + 45 + REL_ARC_TO_SELF[player.name][0] * 1.5))
+        screen.blit(legend, (left + 50, top + 60 + REL_ARC_TO_SELF[player.name][0] * 1.5))
         w_left = left + HAS_CARD_DICT[player.name][0] * width
         w_top = top + HAS_CARD_DICT[player.name][1] * height
         world_node = pygame.Rect(w_left, w_top, 50, 50)
@@ -62,6 +65,8 @@ def draw_model(screen, players, font, card, left, top, width, height):
         for relation in relations:
 
             if true_card_owner == relation[0]:
+                # we draw the relation the players have between the true world and itself, for clarity (if one player knows the
+                # owner of the card, that would otherwise mean no relations are drawn for that player for that card)
                 w_left = left + HAS_CARD_DICT[true_card_owner][0] * width - REL_ARC_TO_SELF[player.name][0] + REL_ARC_TO_SELF[true_card_owner][1]
                 w_top = top + HAS_CARD_DICT[true_card_owner][1] * height - REL_ARC_TO_SELF[player.name][0] + REL_ARC_TO_SELF[true_card_owner][2]
 
@@ -76,6 +81,35 @@ def draw_model(screen, players, font, card, left, top, width, height):
                 start_pos = (w_start_l, w_start_t)
                 end_pos = (w_stop_l, w_stop_t)
                 pygame.draw.line(screen, REL_COLOR_DICT[player.name], start_pos, end_pos, 2)
+        if len(relations) == 3:
+            # there are three worlds which are held for possible by a player concerning the ownership of a card, at this point
+            # we drew the relation between the true world and false world A and between the true world and false world B, we
+            # can infer (because of the euclidean property of relations in S5 models) that there is a relation between false worlds
+            # A and false world B as well, so we draw it. Note that three is the maximum possible nr of words for a single card,
+            # because a player always knows their own cards.
+            start_defined = False
+            for relation in relations:
+                if relation[0] != true_card_owner and not start_defined:
+                    start = relation[0]
+                    start_defined = True
+                if relation[0] != true_card_owner and start_defined:
+                    stop = relation[0]
+            w_start_l = left + HAS_CARD_DICT[start][0] * width + (
+            REL_LINE_START[player.name][0] * REL_LINE_START[start][3][stop][0]) + \
+                        REL_LINE_START[start][1]
+            w_start_t = top + HAS_CARD_DICT[start][1] * height + (
+            REL_LINE_START[player.name][0] * REL_LINE_START[start][3][stop][1]) + \
+                        REL_LINE_START[start][2]
+            w_stop_l = left + HAS_CARD_DICT[stop][0] * width + (
+            REL_LINE_START[player.name][0] * REL_LINE_START[stop][3][start][0]) + \
+                       REL_LINE_START[stop][1]
+            w_stop_t = top + HAS_CARD_DICT[stop][1] * height + (
+            REL_LINE_START[player.name][0] * REL_LINE_START[stop][3][start][1]) + \
+                       REL_LINE_START[stop][2]
+            start_pos = (w_start_l, w_start_t)
+            end_pos = (w_stop_l, w_stop_t)
+            pygame.draw.line(screen, REL_COLOR_DICT[player.name], start_pos, end_pos, 2)
+
 
 
 
